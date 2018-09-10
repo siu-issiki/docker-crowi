8fcef3d2c1b47affbb3cbf039aa13c6c0a3ee6fb
Tomoki Tanezaki <siu.issiki@gmail.com>
2018-09-10 17:32:56 +0900

    use bucket env

--- backup/s3_dump_upload.sh
+++ backup/s3_dump_upload.sh
@@ -4,4 +4,4 @@ dir=`date '+%Y-%m'`
 date=`date '+%Y%m%d'`
 mongodump --host db --port 27017 -d crowi -o crowi.dump
 zip crowi_dump -r crowi.dump
-aws s3 cp crowi_dump.zip s3://spectra-inc/crowi/$dir/$date --storage-class REDUCED_REDUNDANCY
+aws s3 cp crowi_dump.zip s3://$bucket/crowi/$dir/$date --storage-class REDUCED_REDUNDANCY
